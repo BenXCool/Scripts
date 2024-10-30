82ec7c39578f2fb22c982f7d00476a2eefaa5e75
BenXCool_ <147831548+BenXCool@users.noreply.github.com>
2024-10-30 16:32:02 +0100

    Create AbyssalEngineREAL.lua

---
+++ AbyssalEngineREAL.lua
@@ -0,0 +1,14 @@
+local prevRoom = workspace.IntroRoom
+
+for i = 1, 100 do
+	local tablee = workspace.Rooms:GetChildren()
+	local sigmaTable = {}
+	for _, v in tablee do
+		table.insert(sigmaTable, v.Name)
+	end
+	local randomNumber = math.random(1, #tablee)
+	local clonedRoom = workspace.Rooms:FindFirstChild(sigmaTable[randomNumber]):Clone()
+	clonedRoom.Parent = workspace
+	clonedRoom:PivotTo(prevRoom.Exit.CFrame)
+	prevRoom = clonedRoom
+end
